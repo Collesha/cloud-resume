1b9752093cdafb05c61857beadcf25dba7a6d07e
Collesha Mighty <c.mighty21@gmail.com>
2026-05-20 12:16:50 -0400

    feat: add visitor count functionality with API integration

---
+++ frontend/counter.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', () => {
+    getVisitorCount();
+});
+
+const functionApi = "https://mge5qn8ttd.execute-api.YOUR-REGION.amazonaws.com/get-count"; 
+
+function getVisitorCount() {
+    let count = 0;
+    fetch(functionApi)
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            console.log("API Gateway response received:", response);
+            count = response.count;
+            document.getElementById('counter').innerText = count;
+        })
+        .catch(function(error) {
+            console.log("Error fetching visitor count:", error);
+            document.getElementById('counter').innerText = "error";
+        });
+}
