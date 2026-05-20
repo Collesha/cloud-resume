50e3d73ca13b87806831ba505322a3870679f7ae
Collesha Mighty <c.mighty21@gmail.com>
2026-05-20 12:23:21 -0400

    fix: update API endpoint to use the correct region

--- frontend/counter.js
+++ frontend/counter.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     getVisitorCount();
 });
 
-const functionApi = "https://mge5qn8ttd.execute-api.us-east-2.amazonaws.com/get-count"; 
+const functionApi = "https://mge5qn8ttd.execute-api.us-east-1.amazonaws.com/get-count"; 
 
 function getVisitorCount() {
     let count = 0;
