d2d86826ee46644a5a2dbe6744a2ea82ad3fbedb
Collesha Mighty <c.mighty21@gmail.com>
2026-05-20 12:55:01 -0400

    feat: add initial Terraform configuration for AWS DynamoDB table

---
+++ backend/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 1. DynamoDB Table Definition
+resource "aws_dynamodb_table" "visitor_counter" {
+  name         = "cloud-resume-counter"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
